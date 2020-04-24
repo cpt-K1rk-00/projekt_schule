504048e9783a1a04377494bb847a256c8f34ab9e
the_blaggy$ <the.blaggy@gmail.com>
2020-04-24 10:54:58 +0200

    Tree

--- src/server/Bot.java
+++ src/server/Bot.java
@@ -10,15 +10,17 @@ package server;
  */
 public class Bot extends Player
 {
+	TreeMaker mkr = new TreeMaker();
     public Bot()
     {
     	super("Bot", null);
+    	mkr.startAddingLeafs();
     }
     
     void public turn(Board board) {
     	for (int y = 0; y < 3; y++) {
     		for (int x = 0; x < 3; x++) {
-    			if board[y][x] == '#' {
+    			if mkr.getData().getBaord()[y][x] == '#' {
     					board[y][x] = 'o';
     			}
     		}
